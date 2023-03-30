be3f75634af46a201ea251727e38c880cd5855fc
tekostyshyn <tekostyshyn@gmail.com>
2023-03-30 22:29:35 +0300

    gallery

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,83 @@
 <template>
-  <the-header></the-header>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="container">
+    <the-header></the-header>
+    <div v-if="images.length" class="gallery">
+      <button class="gallery__button" @click="decrementPage">Prev
+      </button>
+      <image-carousel :imagesList="images"></image-carousel>
+      <button class="gallery__button" @click="incrementPage">Next
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue'
+import TheHeader from './components/TheHeader.vue';
+import ImageCarousel from './components/ImageCarousel.vue';
 
 export default {
   name: 'App',
   components: {
-    TheHeader
+    TheHeader,
+    ImageCarousel
+  },
+  data() {
+    return {
+      page: 1,
+      imagesPerPage: 4,
+      images: [],
+      areButtonsShowed: true,
+    }
+  },
+  methods: {
+    incrementPage() {
+      this.page++;
+      this.fetchImages();
+    },
+    decrementPage() {
+      this.page--;
+      this.fetchImages();
+    },
+    fetchImages() {
+      fetch(`https://picsum.photos/v2/list?page=${this.page}&limit=4`).then(res => res.json())
+        .then(data => this.images = data)
+        .catch(error => console.log(error));
+    }
+  },
+  mounted() {
+    this.fetchImages();
   }
 }
 </script>
 
 <style>
+li,
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  padding: 0 15px;
+  width: 1280px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.gallery {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  min-height: 180px;
+}
+
+.gallery__button {
+  min-height: 20px;
+  width: 60px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul class="gallery__list">
+        <li v-for="image in imagesList" :key="image.id" class="gallery__item">
+            <img class="gallery__image" :src="image.download_url" alt="">
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'ImageCarousel',
+    props: {
+        imagesList: Array,
+    }
+}
+</script>
+
+<style scoped>
+.gallery__list {
+    display: flex;
+    justify-content: space-between;
+    gap: 20px;
+}
+.gallery__image {
+    width: 270px;
+}
+</style>
